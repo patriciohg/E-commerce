22addb30ae05dee120d28a1c0c536ee98ced8b62
Patricio Hinojosa <patriciohg97@gmail.com>
2023-12-29 02:15:15 -0300

    Se añade ruta de dashboard

---
+++ ecommerce/src/app/dashboard/(root)/page.tsx
@@ -0,0 +1,20 @@
+import { UserButton, auth, redirectToSignIn } from '@clerk/nextjs'
+import { User } from '@clerk/nextjs/server'
+import { redirect } from 'next/dist/server/api-utils';
+import React from 'react'
+
+function Page() {
+    const{ userId } = auth();
+    
+    if(!userId)
+        return redirectToSignIn();
+
+  return (
+    <div>
+        <UserButton afterSignOutUrl="/"/>
+        Hola  mundo
+    </div>
+  )
+}
+
+export default Page
---
+++ ecommerce/src/app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function DashboardLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+  return (
+    <>
+
+        {children}
+    </>
+  )
+}
+
+export default DashboardLayout
